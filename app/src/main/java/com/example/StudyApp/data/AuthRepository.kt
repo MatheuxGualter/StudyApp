20ebe9e791b489c8b8ebf8dcbad5828a171ba0a3
Matheus Gualter <matheusgualtersilvaresende@gmail.com>
2025-09-18 19:54:47 -0300

    API Key teste

--- app/src/main/java/com/example/StudyApp/data/AuthRepository.kt
+++ app/src/main/java/com/example/StudyApp/data/AuthRepository.kt
@@ -1,7 +1,6 @@
 package com.example.StudyApp.data
 
 import android.content.Context
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
--- app/src/main/java/com/example/StudyApp/data/UserLocationDao.kt
+++ app/src/main/java/com/example/StudyApp/data/UserLocationDao.kt
@@ -1,6 +1,5 @@
 package com.example.StudyApp.data
 
-import androidx.lifecycle.LiveData
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
